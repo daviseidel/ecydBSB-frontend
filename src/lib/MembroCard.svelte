c402fceccde638b07852fbca1c272b0e2c7485d8
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-05-28 13:27:37 -0300

    Add logica do PocketBase

--- src/lib/MembroCard.svelte
+++ src/lib/MembroCard.svelte
@@ -1,7 +1,14 @@
 <script>
-  export let nome; 
+  export let nome;
+  export let descricao;
 </script>
-  <div class="card flex-row space-y-3">
-    <header class="card-header py-2">Acampamento ECyD de Inverno 2024</header>
-    <a class="block card-hover p-4 btn variant-filled" href="/elements/cards">Se Inscreva</a>
+
+<html lang="br">
+  <div class="lg:px-20 md:px-20 sm:px-10 my-1">
+    <div class="card flex-row space-y-3">
+      <header class="card-header py-2">{nome}</header>
+      <span class="my-5 py-1 font-light blockquote">{descricao}</span>
+      <a class="block card-hover p-4 btn variant-filled" href="/elements/cards">Se Inscreva</a>
+    </div>
   </div>
+</html>
--- src/routes/eventos/+page.svelte
+++ src/routes/eventos/+page.svelte
@@ -1,7 +1,20 @@
-<div class="px-20 my-1">
-  <div class="card flex-row space-y-3">
-    <header class="card-header py-2">Acampamento ECyD de Inverno 2024</header>
-    <span class="my-5 py-1 font-light blockquote">Teste para entender como uma descrição pode ser feita em um evento de maneira automática</span>
-    <a class="block card-hover p-4 btn variant-filled" href="/elements/cards">Se Inscreva</a>
-  </div>
-</div>
+<script lang="ts">
+  import MembroCard from "$lib/MembroCard.svelte";
+  import PocketBase from 'pocketbase';
+  import {onMount} from 'svelte';
+
+  // Initialize PocketBase
+  const url = 'https://ecyd-bsb.pockethost.io/'
+  const pb = new PocketBase(url)
+  let records = {}
+  onMount(async () => {
+     records = await pb.collection('eventos').getFullList({
+      sort: '-created',
+    });
+    console.log(records)
+  });
+  
+</script>
+ 
+<MembroCard nome="Acampamento ECyD de Inverno 2024" descricao="Teste para entender como uma descrição pode ser feita em um evento de maneira automática" />
+
