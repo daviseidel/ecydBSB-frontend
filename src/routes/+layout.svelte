a38399fcd8006f056537bfa57ac89b8d498a4cdd
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-05-30 17:04:53 -0300

    Add login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar, LightSwitch, Avatar } from '@skeletonlabs/skeleton';
-  import { pb, currentUser } from '../stores/auth.ts';
+  import { pb, currentUser, login, logout } from '../stores/auth.ts';
+  // Popup
+  import { popup } from '@skeletonlabs/skeleton';
+  import type { PopupSettings } from '@skeletonlabs/skeleton';
+  import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+  import { storePopup } from '@skeletonlabs/skeleton';
+  storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow }); 
+  const popupClick: PopupSettings = {
+    event: 'click',
+    target: 'popupClick',
+    placement: 'bottom'
+  };
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     await login();
   };
 
-  let user;
-  $: currentUser.subscribe(value => user = value);
 
-  async function login() {
-  try {
-    const authData = await pb.collection('membros').authWithOAuth2({ provider: 'google' });
-    console.log(pb.authStore.isValid);
-    // "logout" the last authenticated model
-    pb.authStore.clear();
-
-  } catch (error) {
-    console.error('Login failed', error);
-  }
-}
-
-
+  let user = pb.authStore.model;
+ 
+  console.log(pb.authStore.model)
 </script>
 
 <!-- App Shell -->
@@ -32,25 +32,58 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong> <a class="text-xl uppercase" href="/">ECyD BSB</a></strong>
-        				<strong> <a class="text-xl uppercase" href="/">{user}</a></strong>
-
+				<strong> <a class="text-xl uppercase" href="/">ECyD BSB </a></strong>
 			</svelte:fragment>
+
       <svelte:fragment slot="trail">
         <LightSwitch />
-        <a on:click={login} class="card-hover rounded-full">
-          {#if pb.authStore.isValid}
-            <Avatar src="https://ecyd-bsb.pockethost.io/api/files/membros/{pb.authStore.user.id}/{pb.authStore.user.imagem}" initials="s" width="w-9"/>
+        <button use:popup={popupClick} class="card-hover rounded-full">
+          {#if user}
+            <Avatar src="https://ecyd-bsb.pockethost.io/api/files/membros/{user.id}/{user.imagem}" initials="s" width="w-9"/>
           {:else}
             <Avatar initials="A" width="w-9"/>
           {/if} 
-            
-          <!-- <Avatar initials="A" width="w-9"/> -->
-        </a>
-			</svelte:fragment>
+        </button>
+			</svelte:fragment>      
+      {#if user}
+      <div class="card p-4 w-72 shadow-xl items-end flex justify-items-end" data-popup="popupClick">
+        <div class="space-y-4 place-content-end justify-items-end">
+						<Avatar src="https://ecyd-bsb.pockethost.io/api/files/membros/{user.id}/{user.imagem}" width="w-16" />
+						<div>
+							<p class="font-bold">{user.nome}</p>
+							<p class="opacity-50">{user.etapa}° Etapa</p>
+						</div>
+            <div class="grid">
+              <button class="btn variant-soft">Eventos Inscritos</button>
+              <button class="btn variant-soft">Dados</button>
+            </div>
+						<div class="flex gap-4">
+							<small><strong>{2024 - user.anoDeAdesao}</strong> <span class="opacity-50">Anos no ECyD</span></small>
+							<small><strong>{0 }</strong> <span class="opacity-50">Eventos</span></small>
+						</div>
+						<a class="btn variant-filled-error w-full" on:click={logout}> Sair </a>
+				</div>
+				<div class="arrow bg-surface-100-800-token"></div>
+      </div>
+      {:else}
+       <div class="card p-4 w-72 shadow-xl items-end flex justify-items-end" data-popup="popupClick">
+        <div class="space-y-4 place-content-end justify-items-end">
+						<div class="grid">
+              <button class="btn variant-soft" on:click={login}>Login</button>
+              <button class="btn variant-soft" on:click={login}>Se Inscrever</button>
+            </div>
+				</div>
+				<div class="arrow bg-surface-100-800-token"></div>
+      </div>     
+      {/if}
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
   
 </AppShell>
+
+<!-- popup -->
+
+
+
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -8,8 +8,24 @@ export const pb = new PocketBase(url)
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
+  console.log(auth)  
   currentUser.set(pb.authStore.model);
 });
+
+export async function login() {
+  try {
+    const authData = await pb.collection('membros').authWithOAuth2({provider: 'google'});
+    console.log(pb.authStore.isValid);
+    console.log(pb.authStore.model)  
+    currentUser.set(pb.authStore.model);
+    // "logout" the last authenticated model
+    // pb.authStore.clear();
+
+  } catch (error) {
+    console.error('Login failed', error);
+  }
+}
+
 
 export async function logout() {
   pb.authStore.clear();
